2f522daa167f83268e620e5dac14ff8fa04bd4a9
Nilushiya <nilushiyak@gmail.com>
2023-09-09 07:17:41 +0530

    navbar style

--- home.php
+++ home.php
@@ -11,20 +11,22 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <section class="header">
-        <a href="home.php">Travel.</a>
+    <section>
+        <div class="header">
+            <a href="home.php" class="logo">Travel.</a>
 
-        <nav class="navbar">
-            <a href="home.php">home</a>
-            <a href="about.php">about</a>
-            <a href="package.php">package</a>
-            <a href="book.php">book</a>
-        </nav>
+            <nav class="navbar">
+                <a href="home.php">home</a>
+                <a href="about.php">about</a>
+                <a href="package.php">package</a>
+                <a href="book.php">book</a>
+            </nav>
+
+            <div id="menu-btn" class="fas fa-bars"></div>
+        </div>
+
     </section>
-    <?php 
-    $a =12;
-    echo $a;
-    ?>
+
     <!--swiper js-->
     <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
     <script src="script.js"></script>

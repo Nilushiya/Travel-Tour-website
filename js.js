a9c9a70b20ec8dbd84045a33fd3b7b578ed3d49f
Nilushiya <nilushiyak@gmail.com>
2023-09-13 18:15:34 +0530

    connect Swiper

--- js.js
+++ js.js
@@ -1,8 +1,16 @@
+let swiper = new Swiper(".home-slider", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 let menu = document.querySelector("#menu-btn");
 let  navbar = document.querySelector(".header .navbar");
 
 
 menu.addEventListener("click",() => {
-    menu.classList.toggle("fa-times");
-    navbar.classList.toggle("active");
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
 })
+

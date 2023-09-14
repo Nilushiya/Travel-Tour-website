00ff093206dd370276dac3c34291024eaf4069c5
Nilushiya <nilushiyak@gmail.com>
2023-09-14 08:18:51 +0530

    Build about page

--- js.js
+++ js.js
@@ -14,3 +14,23 @@ menu.addEventListener("click",() => {
   navbar.classList.toggle("active");
 })
 
+
+
+let swiperr = new Swiper(".reviews-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoHeight:true,
+  grabCursor:true,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
